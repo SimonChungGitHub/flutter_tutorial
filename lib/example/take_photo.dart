f62fa9df16ec26cbd306123c3fa62159eba5f604
chung <chungjenching@gmail.com>
2023-07-30 03:41:18 +0800

    照片縮放

--- lib/example/take_photo.dart
+++ lib/example/take_photo.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 import 'package:camera_camera/camera_camera.dart';
 import 'package:dio/dio.dart';
+import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sn_progress_dialog/progress_dialog.dart';
+import 'package:zoom_pinch_overlay/zoom_pinch_overlay.dart';
 
 import '../config.dart';
 
@@ -20,6 +22,8 @@ class _TakePhotoExampleState extends State<TakePhotoExample> {
   late Future<void> _initializeControllerFuture;
   late CameraDescription firstCamera;
   File? image;
+  bool zoomMode = false;
+  String mode = 'easy_image_viewer';
 
   @override
   void initState() {
@@ -54,102 +58,116 @@ class _TakePhotoExampleState extends State<TakePhotoExample> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          ///camera_camera
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(10),
-            child: ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.blue[900]),
-                child: const Text('camera_camera'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => CameraCamera(
-                        onFile: (file) {
-                          Navigator.pop(context);
-                          setState(() => image = file);
-                          _showSnackBar(file.parent);
-                        },
-                      ),
-                    ),
-                  );
-                }),
-          ),
-
-          ///image_picker
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(10),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text('image_picker'),
-                onPressed: () {
-                  final picker = ImagePicker();
-                  picker
-                      .pickImage(source: ImageSource.camera)
-                      .then((value) async {
-                    _showLoadingDialog();
-                    await Future.delayed(const Duration(seconds: 2));
-                    setState(() => image = File(value!.path));
-                    await Future.delayed(const Duration(seconds: 1));
-                    setState(() => Navigator.pop(context));
-                  });
-                }),
-          ),
-
-          ///自製相機
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(10),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text('自製相機'),
-                onPressed: () {
-                  WidgetsFlutterBinding.ensureInitialized();
-                  availableCameras().then((cameras) {
-                    firstCamera = cameras.first;
-                    _controller = CameraController(
-                      firstCamera,
-                      ResolutionPreset.high,
-                    );
-                    _initializeControllerFuture = _controller.initialize();
-                    Navigator.push(
+          Row(
+            children: [
+              ///camera_camera
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue[900]),
+                    child: const Text('camera_camera'),
+                    onPressed: () {
+                      Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => MaterialApp(
-                            theme: ThemeData.dark(),
-                            home: takePhotoScreen(),
+                          builder: (_) => CameraCamera(
+                            onFile: (file) {
+                              Navigator.pop(context);
+                              setState(() => image = file);
+                              _showSnackBar(file.parent);
+                            },
                           ),
-                        ));
-                  });
-                }),
-          ),
+                        ),
+                      );
+                    }),
+              ),
 
-          ///dioUpload
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(10),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text('上傳'),
-                onPressed: () => _dioUpload()),
+              ///image_picker
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    child: const Text('image_picker'),
+                    onPressed: () {
+                      final picker = ImagePicker();
+                      picker
+                          .pickImage(source: ImageSource.camera)
+                          .then((value) async {
+                        _showLoadingDialog();
+                        await Future.delayed(const Duration(seconds: 2));
+                        setState(() => image = File(value!.path));
+                        await Future.delayed(const Duration(seconds: 1));
+                        setState(() => Navigator.pop(context));
+                      });
+                    }),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              ///自製相機
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    child: const Text('自製相機'),
+                    onPressed: () {
+                      WidgetsFlutterBinding.ensureInitialized();
+                      availableCameras().then((cameras) {
+                        firstCamera = cameras.first;
+                        _controller = CameraController(
+                          firstCamera,
+                          ResolutionPreset.high,
+                        );
+                        _initializeControllerFuture = _controller.initialize();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MaterialApp(
+                                theme: ThemeData.dark(),
+                                home: takePhotoScreen(),
+                              ),
+                            ));
+                      });
+                    }),
+              ),
+
+              ///dioUpload
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    child: const Text('上傳'),
+                    onPressed: () => _dioUpload()),
+              ),
+              Text(mode),
+              Switch(
+                  value: zoomMode,
+                  onChanged: (value) {
+                    setState(() {
+                      zoomMode = !zoomMode;
+                      if (zoomMode) {
+                        mode = 'easy_image_viewer';
+                      } else {
+                        mode = 'zoom_pinch_overlay';
+                      }
+                    });
+                  }),
+            ],
           ),
           Expanded(
             child: Container(
                 alignment: Alignment.topCenter,
                 margin: const EdgeInsets.all(10),
-                child: ClipRRect(
-                  ///是 ClipRRect，不是 ClipRect
-                  borderRadius: BorderRadius.circular(12),
-                  child: image == null
-                      ? null
-                      : Image.file(
-                          image!,
-                          fit: BoxFit.cover,
-                        ),
-                )),
+                child: zoomMode ? easyImageViewer() : zoomPinchOverlay()),
           ),
         ],
       ),
@@ -188,6 +206,39 @@ class _TakePhotoExampleState extends State<TakePhotoExample> {
         },
         child: const Icon(Icons.camera_alt),
       ),
+    );
+  }
+
+  Widget easyImageViewer() {
+    return GestureDetector(
+      onTap: () {
+        showImageViewerPager(
+          context,
+          SingleImageProvider(Image.file(image!).image),
+          doubleTapZoomable: true,
+        );
+      },
+      child: ClipRRect(
+        ///是 ClipRRect，不是 ClipRect
+        borderRadius: BorderRadius.circular(12),
+        child: image == null ? null : Image.file(image!),
+      ),
+    );
+  }
+
+  Widget zoomPinchOverlay() {
+    return ZoomOverlay(
+      modalBarrierColor: Colors.black12,
+      minScale: 0.8,
+      maxScale: 1.8,
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: const Duration(milliseconds: 300),
+      twoTouchOnly: true,
+      onScaleStart: () {},
+      onScaleStop: () {},
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: image == null ? null : Image.file(image!)),
     );
   }
 
