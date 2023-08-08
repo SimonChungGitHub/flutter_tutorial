a0535b284060d9f6610bc54a642930509ff8d0e4
chung <chungjenching@gmail.com>
2023-08-08 22:19:15 +0800

    使用 camera 拍照, 使用 gallery_saver 存入相簿

--- lib/example/custom_camera.dart
+++ lib/example/custom_camera.dart
@@ -4,15 +4,16 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 
-import '../utils.dart';
-
+///使用 camera 拍照, 使用 gallery_saver 存入相簿
 class CustomCamera extends StatefulWidget {
-  const CustomCamera(this.initializeControllerFuture, this._controller, {super.key});
+  const CustomCamera(this._initializeControllerFuture, this._controller,
+      {super.key});
 
-  final Future<void> initializeControllerFuture;
+  final Future<void> _initializeControllerFuture;
   final CameraController _controller;
 
   @override
@@ -22,6 +23,7 @@ class CustomCamera extends StatefulWidget {
 class CustomCameraState extends State<CustomCamera> {
   late CameraDescription camera;
   late File image;
+  DateTime? lastPopTime;
 
   @override
   void initState() {
@@ -59,18 +61,22 @@ class CustomCameraState extends State<CustomCamera> {
   Widget portrait(context) {
     return Column(
       children: [
-        FutureBuilder<void>(
-          future: widget.initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return CameraPreview(widget._controller);
-            } else {
-              // Otherwise, display a loading indicator.
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
+        Expanded(
+          flex: 85,
+          child: FutureBuilder<void>(
+            future: widget._initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return CameraPreview(widget._controller);
+              } else {
+                // Otherwise, display a loading indicator.
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
         Expanded(
+          flex: 15,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -79,33 +85,7 @@ class CustomCameraState extends State<CustomCamera> {
                     Icons.camera_alt,
                   ),
                   iconSize: 60,
-                  onPressed: () async {
-                    try {
-                      ///相機快門音效
-                      final player = AudioPlayer();
-                      await player.play(AssetSource('snapshot.mp3'));
-
-                      ///碼表計時
-                      final Stopwatch stopwatch = Stopwatch();
-                      stopwatch.start();
-
-                      await widget.initializeControllerFuture;
-                      String newPath =
-                          '${(await getTemporaryDirectory()).path}/${DateFormat('yyyyMMdd_HHmmss_SSS').format(DateTime.now())}.jpg';
-                      widget._controller.setFocusMode(FocusMode.locked);
-                      final xFile = await widget._controller.takePicture();
-
-                      image = await File(xFile.path).rename(newPath);
-                      debugPrint('\u001b[31m ${image.path} \u001b[0m');
-                      debugPrint(
-                          '\u001b[31m ${stopwatch.elapsedMicroseconds / 1000} ms \u001b[0m');
-                      stopwatch.stop();
-                    } on Exception catch (_) {
-                      showSnackBar(context, _.toString());
-                    } finally {
-                      // Navigator.pop(context); //離開相機
-                    }
-                  }),
+                  onPressed: () => onPressAndTakePhoto()),
             ],
           ),
         ),
@@ -116,18 +96,22 @@ class CustomCameraState extends State<CustomCamera> {
   Widget landscape(context) {
     return Row(
       children: [
-        FutureBuilder<void>(
-          future: widget.initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return CameraPreview(widget._controller);
-            } else {
-              // Otherwise, display a loading indicator.
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
+        Expanded(
+          flex: 85,
+          child: FutureBuilder<void>(
+            future: widget._initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return CameraPreview(widget._controller);
+              } else {
+                // Otherwise, display a loading indicator.
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
         Expanded(
+          flex: 15,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -136,38 +120,43 @@ class CustomCameraState extends State<CustomCamera> {
                     Icons.camera_alt,
                   ),
                   iconSize: 60,
-                  onPressed: () async {
-                    try {
-                      ///相機快門音效
-                      final player = AudioPlayer();
-                      await player.play(AssetSource('snapshot.mp3'));
-
-                      ///碼表計時
-                      final Stopwatch stopwatch = Stopwatch();
-                      stopwatch.start();
-
-                      await widget.initializeControllerFuture;
-                      String newPath =
-                          '${(await getTemporaryDirectory()).path}/${DateFormat('yyyyMMdd_HHmmss_SSS').format(DateTime.now())}.jpg';
-                      widget._controller.setFocusMode(FocusMode.locked);
-                      final xFile = await widget._controller.takePicture();
-
-                      if (!mounted) return;
-                      image = await File(xFile.path).rename(newPath);
-                      debugPrint('\u001b[31m ${image.path} \u001b[0m');
-                      debugPrint(
-                          '\u001b[31m ${stopwatch.elapsedMicroseconds / 1000} ms \u001b[0m');
-                      stopwatch.stop();
-                    } on Exception catch (_) {
-                      showSnackBar(context, _.toString());
-                    } finally {
-                      // Navigator.pop(context); //離開相機
-                    }
-                  }),
+                  onPressed: () => onPressAndTakePhoto()),
             ],
           ),
         ),
       ],
     );
+  }
+
+  Future<void> onPressAndTakePhoto() async {
+    if (lastPopTime == null ||
+        DateTime.now().difference(lastPopTime!) > const Duration(seconds: 1)) {
+      try {
+        await widget._initializeControllerFuture;
+        //Error: select a camera first.
+        if (!widget._controller.value.isInitialized) return;
+        // // A capture is already pending, do nothing.
+        if (widget._controller.value.isTakingPicture) return;
+        widget._controller.setFocusMode(FocusMode.locked);
+        final xFile = await widget._controller.takePicture();
+        if (!mounted) return;
+
+        ///相機快門音效
+        final player = AudioPlayer();
+        await player.play(AssetSource('snapshot.mp3'));
+
+        String newPath =
+            '${(await getTemporaryDirectory()).path}/${DateFormat('yyyyMMdd_HHmmss_SSS').format(DateTime.now())}.jpg';
+        image = await File(xFile.path).rename(newPath);
+        debugPrint('\u001b[31m ${image!.path} \u001b[0m');
+        ///相片存入相簿後刪除檔案
+        await GallerySaver.saveImage(image.path);
+        image.delete();
+        setState(() {});
+      } on Exception catch (_) {
+        debugPrint('\u001b[31m ${_.toString()} \u001b[0m');
+      }
+      lastPopTime = DateTime.now();
+    }
   }
 }
