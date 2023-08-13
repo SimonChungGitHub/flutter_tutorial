5bf0563e1d3a5a9780aeca7c25aa1e524ad0f04f
chung <chungjenching@gmail.com>
2023-08-13 23:53:20 +0800

    檔案目錄操作

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:android_id/android_id.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 String identifierToHex(var identifier) {
@@ -85,4 +88,39 @@ void showSnackBar(context, text) {
     ),
   );
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+/// use path_provider package
+Future<void> createDir() async {
+  Directory directory = await getTemporaryDirectory();
+  String path = '${directory.path}${Platform.pathSeparator}';
+  var dir = Directory(path);
+  if (dir.existsSync()) {
+    debugPrint('\u001b[31m 資料夾已存在 $path \u001b[0m');
+  } else {
+    await dir.create();
+    debugPrint('\u001b[31m 建立資料夾 $path \u001b[0m');
+  }
+}
+
+///取得指定目錄下所有檔案
+Future<List<File>> dirList() async {
+  Directory directory = await getTemporaryDirectory();
+  String path = '${directory.path}${Platform.pathSeparator}';
+  Stream<FileSystemEntity> list = Directory(path).list();
+  List<File> files = [];
+  await for (FileSystemEntity entity in list) {
+    debugPrint('\u001b[31m ${entity.toString()} \u001b[0m');
+    if (entity.path.endsWith('jpg')) files.add(File(entity.path));
+  }
+  return files;
+}
+
+Future<void> deleteDir() async {
+  Directory directory = await getTemporaryDirectory();
+  String path = '${directory.path}${Platform.pathSeparator}cache';
+  var dir = Directory(path);
+  if (dir.existsSync()) {
+    dir.delete();
+  }
 }
